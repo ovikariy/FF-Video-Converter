8861c181ce3263331f1d372025f775dde8108c86
Nicolas Chabra <nchabra@gmail.com>
2020-03-21 15:27:32 +0800

    feat: add probing of ffmpeg directory

--- FF Video Converter/App.xaml.cs
+++ FF Video Converter/App.xaml.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
 using System.Windows;
 
 namespace FFVideoConverter
@@ -8,7 +10,28 @@ namespace FFVideoConverter
     {
         public App()
         {
-            Unosquare.FFME.Library.FFmpegDirectory = Environment.CurrentDirectory;
+            if (!TryFindPath(out var ffmpegDirectoryPath))
+                throw new FileNotFoundException("ffmpeg path not found");
+            Unosquare.FFME.Library.FFmpegDirectory = ffmpegDirectoryPath;
+        }
+
+        static bool TryFindPath(out string path)
+        {
+            var paths = new List<string>{
+                Path.GetDirectoryName (typeof(App).Assembly.Location),
+                Environment.CurrentDirectory,
+            };
+            paths.AddRange(Environment.GetEnvironmentVariable("PATH").Split(';'));
+            foreach (var p in paths)
+            {
+                if (!File.Exists(Path.Combine(p, "ffmpeg.exe")))
+                    continue;
+                path = p;
+                return true;
+            }
+
+            path = null;
+            return false;
         }
     }
 }
